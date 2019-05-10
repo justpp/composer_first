fcedddf489723a4364050de4fde5a6fe3e014f20
justpp <1195455487@qq.com>
2019-05-10 17:01:40 +0800

    just first

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-05-10
+ * Time: 16:45
+ */
+require_once './vendor/autoload.php';
+
+use anonymetheone\JustPP;
+
+$JustPP = new JustPP();
+
+echo $JustPP->say();
