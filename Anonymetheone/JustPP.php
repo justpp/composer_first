9b3f990afef494fc0775ee5d87959ea6cb406863
root <root@localhost.localdomain>
2020-10-26 10:17:41 +0800

    refactor

---
+++ Anonymetheone/JustPP.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-05-10
+ * Time: 16:24
+ */
+namespace anonymetheone;
+
+class JustPP
+{
+    public function say($ii = '')
+    {
+        echo 'hello world'.$ii;
+    }
+}
+
+
